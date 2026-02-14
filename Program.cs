377e369459abf35aa36f01eaa5b0cb136b41d927
Frances <secnarfdelacruz@gmail.com>
2026-02-14 17:47:26 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace AccountManagementDelaCruz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Account Management System");
+
+            string usn = "username1234";
+            string pw = "user123";
+
+            Console.WriteLine("Enter Username: ");
+            string usnInput = Console.ReadLine();
+            Console.WriteLine("Enter Password: ");
+            string pwInput = Console.ReadLine();
+
+            if (usnInput == usn && pwInput == pw)
+            {
+                Console.WriteLine("Login Successful.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Credentials");
+            }
+        }
+    }
+}
